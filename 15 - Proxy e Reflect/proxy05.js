f9a2d1b8adbd4fe59810434f203e3b1781e204a7
Paulo Ilarde <pauloilarde@gmail.com>
2019-12-29 16:23:35 -0300

    Entendendo o método get

---
+++ 15 - Proxy e Reflect/proxy05.js
@@ -0,0 +1,34 @@
+function createArray() {
+    return new Proxy({}, {
+        set(target, key, value) {
+            target.length = target.length || 00
+            target.length++;
+            target[key] = value;
+        },
+        get(target, key) {
+            if (typeof key === "string" && key.match(/\d+/)) {
+                if (!(key in target)) {
+                    throw `Property ${key} not found.`;
+                }
+            }
+            return target[key];
+        },
+        deleteProperty(target, key) {
+            if (key in target) {
+                target.length--;
+                delete target[key];
+            }
+        }
+    });
+}
+
+let languages = createArray();
+languages[0] = 'PHP';
+languages[1] = 'Ruby';
+languages[2] = 'Go';
+
+console.log(languages);
+console.log(languages.length);
+console.log(languages[0]);
+console.log(languages[3]);
+
