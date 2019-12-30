c869f2d468b64d10eb615ef7e2571f60c581e369
Paulo Ilarde <pauloilarde@gmail.com>
2019-12-30 16:29:08 -0300

    Melhorando o promise.all

---
+++ 17 - Promises/promises09.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+    return new Promise(function (resolve, reject) {
+        if (!a || !b) return reject('Invalid input!');
+        setTimeout(() => {
+            resolve(a + b);
+        }, 1000);
+    });
+}
+console.time('medindo a performance');
+Promise.all([
+    sum(3, 3),
+    sum(4, 4)
+]).then(function (values) {
+    let [a, b] = values;
+    return sum(a, b).then(function (result) {
+        console.log(result);
+        console.timeEnd('medindo a performance');
+    });
+}).catch(function (e) {
+    console.log(e);
+});
