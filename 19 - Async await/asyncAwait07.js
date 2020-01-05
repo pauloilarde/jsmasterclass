b2bb6958c4ed4b215c9b61f14f5f0375608790a4
Paulo Ilarde <pauloilarde@gmail.com>
2020-01-05 20:23:55 -0300

    Entendendo o for-await-of

---
+++ 19 - Async await/asyncAwait07.js
@@ -0,0 +1,28 @@
+function sum(a, b) {
+    return new Promise(function (resolve, reject) {
+        if (!a || !b) return reject('Invalid input');
+        setTimeout(() => {
+            resolve(a + b);
+        }, 1000);
+    });
+}
+
+(async function () {
+    try {
+        const functions = [
+            sum(2, 3),
+            sum(3, 4)
+        ];
+        const results = [];
+        for await (const result of functions) {
+            results.push(result);
+        }
+
+        const [a, b] = results;
+
+        const result = await sum(a, b);
+        console.log(result);
+    } catch (e) {
+        console.log(e);
+    }
+})();
