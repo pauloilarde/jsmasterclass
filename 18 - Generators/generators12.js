705a93901747f2aa50918b8094aaa8eb27f72835
Paulo Ilarde <pauloilarde@gmail.com>
2020-01-04 22:33:18 -0300

    Implementando protocolo iterable de forma manual primeiro, para depois fazer de forma mais simples

---
+++ 18 - Generators/generators12.js
@@ -0,0 +1,27 @@
+function createIterable(...array) {
+    return {
+        [Symbol.iterator]() {
+            let i = 0;
+            return {
+                next() {
+                    if (i < array.length) {
+                        return {
+                            value: array[i++],
+                            done: false
+                        }
+                    } else {
+                        return {
+                            value: undefined,
+                            done: true
+                        }
+                    }
+                }
+            }
+        }
+    };
+}
+
+const languages = createIterable('Fortran', 'Lisp', 'COBOL');
+for (let language of languages) {
+    console.log(language);
+}
