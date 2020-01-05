eca3ce1995664df323d54b6334e0cd6fed60dfd9
Paulo Ilarde <pauloilarde@gmail.com>
2020-01-04 22:16:51 -0300

    Método throw

---
+++ 18 - Generators/generators10.js
@@ -0,0 +1,22 @@
+function* forever() {
+    let value = 1;
+    while (true) {
+        let reset = yield value++;
+        if (reset) value = 1;
+    }
+}
+
+function today() {
+    let date = new Date();
+    console.log(date);
+}
+
+let foreverGenerator = forever();
+console.log(foreverGenerator.next());
+console.log(foreverGenerator.next());
+console.log(foreverGenerator.next());
+today();
+console.log(foreverGenerator.throw('error'));
+console.log(foreverGenerator.return('end'));
+console.log(foreverGenerator.next(true));
+console.log(foreverGenerator.next());
