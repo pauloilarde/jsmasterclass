def81c35741bf9cfd240729136d5924c58048029
Paulo Ilarde <pauloilarde@gmail.com>
2020-01-04 22:36:36 -0300

    Simplificando o código anterior

---
+++ 18 - Generators/generators13.js
@@ -0,0 +1,15 @@
+function createIterable(...array) {
+    return {
+        *[Symbol.iterator]() {
+            let i = 0;
+            while (i < array.length) {
+                yield array[i++];
+            }
+        }
+    };
+}
+
+const languages = createIterable('Fortran', 'Lisp', 'COBOL');
+for (let language of languages) {
+    console.log(language);
+}
